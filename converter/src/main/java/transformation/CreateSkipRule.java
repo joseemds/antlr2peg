a37ebe0a6a76ec04559c49330e8840259d8bc66e
Jose Eduardo Monteiro <mds.josee@gmail.com>
2026-05-16 21:36:27 -0300

    remove skipRules discovery heuristic

--- converter/src/main/java/transformation/CreateSkipRule.java
+++ converter/src/main/java/transformation/CreateSkipRule.java
@@ -1,11 +1,9 @@
 package transformation;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 import peg.PegGrammar;
-import peg.node.Ident;
 import peg.node.Node;
 import peg.node.Operator;
 import peg.node.Rule;
@@ -21,41 +19,8 @@ public class CreateSkipRule {
 
   public void addSkipRule() {
     if (grammar.getOptions().skipRules.isEmpty()) {
-      List<String> possibleSkipRules =
-          List.of(
-              "WS",
-              "WHITESPACE",
-              "COMMENT",
-              "LINE_COMMENT",
-              "WHITESPACES",
-              "DOC_COMMENT",
-              "NEWLINE",
-              "SPACE",
-              "WHITE_SPACE",
-              "SingleLineComment",
-              "MultiLineComment");
-      List<Node> foundSkipRules = new ArrayList<>();
-      for (Rule r : this.grammar.getRules()) {
-        if (possibleSkipRules.contains(r.name())) {
-          foundSkipRules.add(grammar.mkIdent(r.name()));
-        }
-      }
-      if (foundSkipRules.isEmpty()) return;
-
-      Node rhs =
-          grammar.mkTerm(grammar.mkOrderedChoice(foundSkipRules), Optional.of(Operator.PLUS));
-
-      Rule r = grammar.mkParsingRule("SKIP_", rhs);
-
-      grammar.getOptions().skipRules =
-          Optional.of(
-              foundSkipRules.stream().map(skipR -> (Ident) skipR).map(i -> i.name()).toList());
-
-      grammar.addRule(r);
-
       return;
     }
-    ;
 
     List<Node> skipRules =
         grammar.getOptions().skipRules.get().stream()
